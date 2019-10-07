975b342317e700860ba3e84af8e8b3f5c06028ad
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-10-07 09:34:40 +0800

    Finished command line parser

--- src/SocialNetworkSim.py
+++ src/SocialNetworkSim.py
@@ -4,27 +4,39 @@ import sys
 def interactive():
     print("Interactive")
 
-def simulation():
+def simulation(netfile, eventfile, prob_like, prob_foll):
     print("Simulation")
 
-if __name__ == "__main__":
+def make_parser():
     parser = argparse.ArgumentParser(description="Simulate Social Networks.",
                                      add_help=False, prefix_chars='\0')
-    subparsers = parser.add_subparsers()
+    subparsers = parser.add_subparsers(dest="mode")
 
-    simulation_parser = subparsers.add_parser("-s", help="Run in simulation mode", add_help=False)
-    interactive_parser = subparsers.add_parser("-i", help="Run in interactive mode", add_help=False)
+    simulation_parser = subparsers.add_parser("-s", add_help=False,
+                                              help="Run in simulation mode")
+    interactive_parser = subparsers.add_parser("-i", add_help=False,
+                                               help="Run in interactive mode")
 
     simulation_parser.add_argument('netfile', type=argparse.FileType('r'),
                                    help='File that describes the initial network')
     simulation_parser.add_argument('eventfile', type=argparse.FileType('r'),
                                    help='File that lists all events on the network')
-    simulation_parser.add_argument('prob_like', type=float, help='Probability of liking a post')
+    simulation_parser.add_argument('prob_like', type=float,
+                                   help='Probability of liking a post')
     simulation_parser.add_argument('prob_foll', type=float,
                                    help='Probability of following the original poster of a liked post')
+    return parser, interactive_parser, simulation_parser
 
-    parser.parse_args()
+
+if __name__ == "__main__":
+    parser, interactive_parser, simulation_parser = make_parser()
     if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         interactive_parser.print_help(sys.stderr)
         simulation_parser.print_help(sys.stderr)
+    else:
+        args = parser.parse_args()
+        if args.mode == "-i":
+            interactive()
+        elif args.mode == "-s":
+            simulation(args.netfile, args.eventfile, args.prob_like, args.prob_foll)
